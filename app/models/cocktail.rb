a774bdec927c9b5b77969e2e166c992818dd0a4f
Gabriel Fontes <gabrielferroroque@hotmail.com>
2020-11-22 01:21:51 -0300

    implementation of css items (demo, need refactoring), images added, buttons, etc

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -5,4 +5,5 @@ class Cocktail < ApplicationRecord
   has_one_attached :photo
 
   validates :name, presence: true, uniqueness: true
+  validates :description, presence: true, length: { minimum: 10, allow_nil: false }
 end
